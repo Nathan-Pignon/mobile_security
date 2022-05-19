0e1b24e28349640805041d0275a1c48e9dd353c0
Nathan-Pignon <nathan.pignon79@gmail.com>
2022-05-19 16:48:12 +0200

    init first page

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -7,18 +7,64 @@ import {
     Text,
     useColorScheme,
     View,
+    Image,
+    TouchableOpacity,
     } from 'react-native';
 
 class Home extends React.Component {
 
     render() {
       return(
-        <SafeAreaView>
-            <Text>Bonjour</Text>
-        </SafeAreaView>
+        <View>
+            <Text>Bonjour test</Text>
+            <Image
+            style={styles.logo}
+            source={require('../../assets/logo/logo_ca.png')}
+            />
+            <View style={[styles.buttonView, { backgroundColor: 'blue', borderColor: 'black' }]}>
+                <TouchableOpacity
+                style={styles.button}>
+                <Text style={{ color: 'white', fontSize: 20 }}> Bienvenue </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
       )
     }
 }
+
+const styles = StyleSheet.create({
+    buttonView: {
+      flexDirection:'row',
+      borderWidth: 1,
+      borderRadius: 30,
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+      elevation: 5,
+      fontSize: 20,
+      marginVertical: 10,
+      paddingHorizontal: 20,
+      width: '75%',
+      alignSelf: 'center',
+    },
+    button: {
+      flex: 1,
+      alignItems:'center',
+      justifyContent:'center',
+      paddingVertical: 15,
+    },
+    logo: {
+      width: 200,
+      height: 200,
+      alignSelf: 'center',
+      marginTop: '25%',
+      marginBottom: '15%',
+    },
+  });
 
 export default Home;
  
